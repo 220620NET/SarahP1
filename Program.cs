82379153d48a3c23dd64a1b1249f20cc2c06fa94
Sarah Robinson <slynnrtwo@gmail.com>
2022-06-29 19:04:11 -0700

    Introduction work

--- Program.cs
+++ Program.cs
@@ -4,4 +4,33 @@ using User;
 using Exceptions;
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("       Welcome to S.Lynns Hobby Shop Employee Reimbursement Platform:      ");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Press ENTER to continue:");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Please choose from the following options:");
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("1.) New user");
+Console.WriteLine("2.) Returning user");
+Console.WriteLine("3.) Exit Program");
+
+switch (Console.ReadLine())
+{
+    case "1":
+        Console.WriteLine("We will now create your user profile");
+    //UserProfile 1 = new UserProfile();
+
+
+    break;
+    case "2":
+      Console.WriteLine("Log in page");
+      //LogIn();
+    break;
+    case "3":
+    break;
+    default:
+      Console.WriteLine("Incorrect input, please choose again");
+    break;
+}
--- User.cs
+++ User.cs
@@ -1,1 +1,15 @@
 namespace User;
+
+
+{
+    class UserProfile {
+        public string FirstName;
+        public string LastName;
+        public string UserName;
+        public string Password;
+        public int IdNumber;
+        public bool IsManager;
+    }
+    
+
+}
