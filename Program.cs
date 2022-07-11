0ed80078b889d33547d2420e266e8ce407f8e368
slynnr2 <66879285+slynnr2@users.noreply.github.com>
2022-07-11 18:52:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using ClassLibraryDataAccess;
+using ClassLibrary1;
+
+
+UserProfile user = new UserProfile();
+
+Console.WriteLine("Please enter your first name:");
+string firstName = Console.ReadLine();
+Console.WriteLine("Please enter your last name");
+string lastName = Console.ReadLine();
+Console.WriteLine("Please create a user name");
+string userName = Console.ReadLine();
+Console.WriteLine("Please create a password");
+string password = Console.ReadLine();
+
+bool isSuccessful = user.CreateUser(new UserModel(firstName, lastName, userName, password));
+if (isSuccessful)
+{
+    Console.WriteLine(firstName + " " + lastName + " " + userName + " " + password);
+} else
+{
+    Console.WriteLine("not created");
+}
