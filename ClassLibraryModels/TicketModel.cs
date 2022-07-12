32da4efe190a53dd6dadb71d79bbe672f109f0ae
slynnr2 <66879285+slynnr2@users.noreply.github.com>
2022-07-12 16:19:54 -0500

    Cleaned up

---
+++ ClassLibraryModels/TicketModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModels
+{
+    internal class TicketModel
+    {
+        public int TicketNumber { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public char Status { get; set; }
+    }
+}
---
+++ ClassLibraryModels/UserModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModels
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string isManager { get; set; }
+
+        public UserModel(string firstName, string lastName, string userName, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.userName = userName;
+            this.password = password;
+    
+
+        }
+        public UserModel(int id, string firstName, string lastName, string userName, string password, string isManager)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.userName = userName;
+            this.password = password;
+            this.isManager = isManager;
+
+
+        }
+        public UserModel(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public override string ToString()
+        {
+            return "id: " + this.id + ", first name: " + this.firstName + " last name: " + this.lastName + " user name: " + this.userName + " password: " + this.password + " Manager: " + this.isManager;
+        }
+    }
+}
