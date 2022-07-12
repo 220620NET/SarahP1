1122f5feccc64643dd3efc6623d0cec33166964d
slynnr2 <66879285+slynnr2@users.noreply.github.com>
2022-07-12 16:18:19 -0500

    Cleaned up

---
+++ ClassLibraryDAO/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+    public class ConnectionFactory
+    {
+        private static ConnectionFactory? _instance;
+        private readonly static string _connectionString = "Server=tcp:mysqlserver-sr2.database.windows.net,1433;Initial Catalog=myTestDatabase;Persist Security Info=False;User ID=sqluser;Password=!ThisPass1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+        private ConnectionFactory()
+        {
+
+        }
+        public static ConnectionFactory GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new ConnectionFactory();
+            }
+        
+            return _instance;
+        }
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
---
+++ ClassLibraryDAO/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryModels;
+using Microsoft.Data.SqlClient;
+
+namespace ClassLibraryDAO
+{
+    public class UserRepository
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        public UserRepository()
+        {
+            _connectionFactory = ConnectionFactory.GetInstance();
+        }
+
+        public List<UserModel> GetAllUsers()
+        {
+            List<UserModel> userModel = new List<UserModel>();
+            SqlConnection conn = _connectionFactory.GetConnection();
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from p1.users", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                userModel.Add(new UserModel((int) reader[0], (string) reader[3], (string) reader[4], (string) reader[1], (string) reader[2], (string) reader[5]));
+
+            }
+            reader.Close();
+            conn.Close();
+            return userModel;
+        }
+
+    }
+}
