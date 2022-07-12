b4799200c088396a7b55803c412f55d6fdf8fcdb
slynnr2 <66879285+slynnr2@users.noreply.github.com>
2022-07-12 16:23:26 -0500

    Add files via upload

---
+++ ClassLibraryServices/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryDAO;
+using ClassLibraryModels;
+
+namespace ClassLibraryServices
+{
+    public class UserService
+    {
+        private UserRepository userRepository = new UserRepository();
+
+        public List<UserModel> GetAllUsers()
+        {
+            return userRepository.GetAllUsers();
+        }
+        /*GetByUsername()
+        {
+
+        }
+        GetByID()
+        {
+
+        }*/
+
+    }
+}
